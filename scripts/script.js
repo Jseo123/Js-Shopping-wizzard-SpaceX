7db62b315ce84b77bd787496009c608fa8dc458e
Carla <carlacardozo990@gmail.com>
2021-11-02 14:58:27 +0000

    locura

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 //const elements
 var ImgFirst = document.querySelector(".ImgFirst");
 var minitureImg = document.querySelector(".mySlides");
-var
+var changeColorsImg = document.querySelector(".cohetes");
+const changeSelection2 = document.querySelector(".selection2")
 
-// chance img miniture to First img
+
+// events
 minitureImg.addEventListener("click", change);
+changeColorsImg.addEventListener("click", changeColors);
+changeSelection2.addEventListener("click", selector2)
 
 // change miniture to ImgFirst
 function change (e) {
@@ -13,3 +17,15 @@ function change (e) {
 
 
 // change color ImgFirst
+function changeColors (e) {
+  ImgFirst.src = e.target.src;
+}
+
+function selector2 () {
+  while (changeColorsImg.firstChild) {
+    changeColorsImg.removeChild(changeColorsImg.childNodes[0]);
+  }
+  const img1 = createElement("img")
+  img1.setAttribute("src","guidance images\assets\2_falcon_heavy.jpg")
+  changeColorsImg.appendChild(img1);
+}
