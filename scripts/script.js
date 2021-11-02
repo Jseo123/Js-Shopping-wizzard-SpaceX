80b1152d7482dc86cf10135df3e9a4fd4f6039ab
jseo123 <jose123135@gmail.com>
2021-11-02 13:04:37 +0100

    added page change functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
-const mainProductbtn = document.querySelector(".main-product-button");
+const mainProductbtn = document.querySelector(".btn1");
+const homepage = document.querySelector(".homepage")
 const product1 = document.querySelector(".product1")
 const product2 = document.querySelector(".product2")
 const product3 = document.querySelector(".product3")
 const username = document.querySelector(".username")
 const adress = document.querySelector(".adress")
 const delivery = document.querySelector(".delivery")
-const productBtnPress = mainProductbtn.addEventListener("click", contentChange);
+
+
+mainProductbtn.addEventListener("click", contentChange1);
 
 console.log(mainProductbtn)
 
-
-
-productBtnPress;
-
-function contentChange() {
+function contentChange1() {
+  username.style.display = "block";
+homepage.style.display ="none"
   
+  return;
 }
+
