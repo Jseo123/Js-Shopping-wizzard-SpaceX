986c113d765a04055ac7bd2173e8ef3ea9c71991
Carla <carlacardozo990@gmail.com>
2021-11-04 20:53:32 +0000

    css endBuy

--- scripts/script.js
+++ scripts/script.js
@@ -89,8 +89,6 @@ let price = {
   google: 2000,
   fifa: 20,
 };
-// var buyPhoto = document.getElementById("finalBuy");
-// buyPhoto.src = ImgFirst.src;
 function Order(product, priceProduct, sponsor, priceSponsor) {
   this.product = product;
   this.price = priceProduct;
@@ -132,40 +130,40 @@ function changeMiniature(e) {
       changePhoto1.classList.remove("hide");
       changePhoto1.classList.add("show");
       break;
-    case "second":
-      changePhoto2.classList.remove("hide");
-      changePhoto2.classList.add("show");
-      break;
-    case "thrid":
-      changePhoto3.classList.remove("hide");
-      changePhoto3.classList.add("show");
-      break;
-    case "fourth":
-      changePhoto4.classList.remove("hide");
-      changePhoto4.classList.add("show");
-      break;
-  }
-}
-
-// function to hide colunm R
-function hide() {
-  changePhoto1.classList.remove("show");
-  changePhoto2.classList.remove("show");
-  changePhoto3.classList.remove("show");
-  changePhoto4.classList.remove("show");
-  changePhoto1.classList.add("hide");
-  changePhoto2.classList.add("hide");
-  changePhoto3.classList.add("hide");
-  changePhoto4.classList.add("hide");
-}
-
-// change color ImgFirst
-function changeImg1(e) {
-  ImgFirst.src = e.target.src;
-  currentRocket = e.target.getAttribute("value");
-}
-function changeImg2(e) {
-  ImgFirst.src = e.target.src;
+      case "second":
+        changePhoto2.classList.remove("hide");
+        changePhoto2.classList.add("show");
+        break;
+        case "thrid":
+          changePhoto3.classList.remove("hide");
+          changePhoto3.classList.add("show");
+          break;
+          case "fourth":
+            changePhoto4.classList.remove("hide");
+            changePhoto4.classList.add("show");
+            break;
+          }
+        }
+        
+        // function to hide colunm R
+        function hide() {
+          changePhoto1.classList.remove("show");
+          changePhoto2.classList.remove("show");
+          changePhoto3.classList.remove("show");
+          changePhoto4.classList.remove("show");
+          changePhoto1.classList.add("hide");
+          changePhoto2.classList.add("hide");
+          changePhoto3.classList.add("hide");
+          changePhoto4.classList.add("hide");
+        }
+        
+        // change color ImgFirst
+        function changeImg1(e) {
+          ImgFirst.src = e.target.src;
+          currentRocket = e.target.getAttribute("value");
+        }
+        function changeImg2(e) {
+          ImgFirst.src = e.target.src;
   currentRocket = e.target.getAttribute("value");
 }
 function changeImg3(e) {
@@ -188,6 +186,7 @@ function oneLess() {
 }
 
 function addSelection() {
+  document.getElementById("finalBuy").src = ImgFirst.src;
   if (currentRocket != null) {
     let array = currentRocket.split("-");
     // order.product = array[0];
